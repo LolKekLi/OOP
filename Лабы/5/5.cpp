dac040716dbed03b5caecb5a386a8cd374a6d16a
Kirill <kirillpazitiv2020@gmail.com>
2021-04-20 09:15:00 +0300

    5.0.1

---
+++ Лабы/5/5.cpp
@@ -0,0 +1,324 @@
+﻿#include <fstream>
+#include <iostream>
+#include <windows.h>
+#include <map>
+#include <list>
+using namespace std;
+
+
+class base
+{
+public:
+	int correctInput()
+	{
+		SetConsoleCP(1251);
+		SetConsoleOutputCP(1251);
+		int i;
+		while (true) {
+
+			cin >> i;
+			if (cin.good())
+			{
+				break;
+			}
+			cin.clear();
+			cout << "Error\n";
+			cin.ignore(10, '\n');
+		}
+		return i;
+	}
+
+	char correctInput(char correctChar1, char correctChar2)
+	{
+		char chr;
+		bool flag = true;
+		while (flag)
+		{
+			cin >> chr;
+			if (chr == correctChar1 || chr == correctChar2)
+			{
+				flag = false;
+				return chr;
+			}
+			else
+			{
+				cin.clear();
+				cout << "Error\n";
+				cin.ignore();
+			}
+		}
+	}
+};
+
+class Number
+{	
+public:
+	static int numbr;
+	Number()
+	{
+		numbr ++;
+	}
+	friend bool operator < (const Number& n1, const Number& n2);
+};
+int Number::numbr;
+
+bool operator < (const Number& n1, const Number& n2)
+{
+	return n1.numbr < n2.numbr;
+}
+
+class Participant :public base
+{
+public:
+	string kindOfSport;
+	string name;
+	int age;
+
+	void setInfo(string name, string kindOfSport, int age)
+	{
+		this->name = name;
+		this->kindOfSport = kindOfSport;
+		this->age = age;
+	}
+
+
+
+	friend ostream& operator<< (std::ostream& out, const Participant& point);
+
+	friend bool operator<(const Participant& lhs, const Participant& rhs);
+};
+
+bool operator<(const Participant& lhs, const Participant& rhs)
+{
+	return lhs.age < rhs.age;
+}
+
+ostream& operator<< (std::ostream& out, const Participant& point)
+{
+	out << point.name << endl << "Возраст ";
+	if (point.age == 20 || point.age == 25)
+	{
+		out << point.age << " лет\nВид спорта:";
+	}
+	else
+	{
+		out << point.age << " года\nВид спорта:";
+	}
+	out << point.kindOfSport << endl;
+
+	return out;
+}
+
+int Random(int min, int max)
+{
+	static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
+
+	return static_cast<int>(rand() * fraction * (max - min + 1) + min);
+}
+
+template <typename T>
+void Start(T& listPerson)
+{
+	string name[] = { "Антон","Борис","Вадим","Владислав","Глеб","Денис","Никита","Олег","Павел","Прохор" };
+	string kindOfSport[] = { "Сквош","Керлинг","Триатлон","Вейкбординг","Сноубординг","Борьба","Армрестлинг","Бег" };
+	
+	for (int i = 0; i < 8; i++)
+	{
+		Participant participant;
+		participant.setInfo(name[Random(0, name->size())], kindOfSport[Random(0, kindOfSport->size())], Random(20, 25));
+		listPerson.push_back(participant);
+	}	
+	
+}
+
+template <typename T>
+void Start_1(T& mapPerson)
+{
+	string name[] = { "Антон","Борис","Вадим","Владислав","Глеб","Денис","Никита","Олег","Павел","Прохор" };
+	string kindOfSport[] = { "Сквош","Керлинг","Триатлон","Вейкбординг","Сноубординг","Борьба","Армрестлинг","Бег" };
+	for (int i = 0; i < 8; i++)
+	{
+		Number numb;
+		Participant participant;
+		participant.setInfo(name[Random(0, name->size())], kindOfSport[Random(0, kindOfSport->size())], Random(20, 25));
+		mapPerson.insert(i,make_pair(numb, participant));
+	}
+}
+
+template <typename T>
+void PrintList(const list<T>& lst)
+{
+	int b = 1;
+	for (auto i = lst.cbegin(); i != lst.cend(); ++i, b++)
+	{
+		cout << b << ":" << *i << endl;
+
+	}
+}
+
+template <typename T, typename U>
+void PrintMap(const map<T,U>& map)
+{
+	for (auto i = map.begin(); i != map.end(); ++i )
+	{
+		cout << (*i).first.numbr <<":"<< (*i).second << endl;
+	}
+	
+}
+
+int main()
+{
+#pragma region start
+	base base;
+	setlocale(LC_ALL, "rus");
+	list<Participant> participants;
+	map<Number, Participant> _map;
+	Start(participants);
+	Start_1(_map);
+	PrintMap(_map);
+	//Start_1(_map);
+	/*cout << "Дамы и Господа\n";
+	Sleep(500);
+	cout << "Приветствуем вас на ВЕЛИЧАЙШИХ СОРЕВНОВАНИЯХ В ИСТОРИИ\n";
+	Sleep(500);
+	cout<<"И так участики соревнований\n"<<endl;
+	Sleep(500);
+	PrintList(participants);
+	system("cls");*/
+#pragma endregion
+/*#pragma region функционал
+	bool end = true;
+	while (end)
+	{
+		int temp;
+		int numb;
+
+		string name;
+		string kindOfsport;
+		int age;
+		Participant addParticipants;
+
+		auto begin = participants.begin();
+
+		ofstream out;
+		out.open("res.txt");
+
+		cout << "1 - Добавить участников соревнования\n2 - Удалить участников соревнования\n3 - Изменить информацию об участнике соревнования\n"
+			"4 - Найти участинка соревнования\n5 - Отсортировать участиков\n6 - Зпись информации в файл\n7 - Просмотр списка участиников\n0 - Выход" << endl;
+		temp = base.correctInput();
+		switch (temp)
+		{
+#pragma region case 1 Добовление 
+		case 1:
+			system("cls");
+			cout << "Введите информцию об участинках\nИмя" << endl;
+			cin >> name;
+			cout << "Возраст\n";
+			age = base.correctInput();
+			cout << "Вид спорта\n";
+			cin >> kindOfsport;
+			addParticipants.setInfo(name, kindOfsport, age);
+			cout << "1 - Добавить в начало\n2 -Добавить в конец" << endl;
+			cin >> temp;
+			if (temp == 1)
+			{
+				participants.push_back(addParticipants);
+			}
+			else if (temp == 2)
+			{
+				participants.push_front(addParticipants);
+			}
+			else
+			{
+				cout << "Error\n";
+			}
+			break;
+#pragma endregion
+#pragma region case2 Удаление
+		case 2:
+			system("cls");
+			PrintList(participants);
+			cout << "Введите номер человека которого хотите удалить" << endl;
+			numb = base.correctInput();
+			if (numb == 1)
+			{
+				participants.pop_front();
+			}
+			else if (numb == participants.size())
+			{
+				participants.pop_back();
+			}
+			else
+			{
+				for (int i = 0; i < numb - 1; i++, begin++);
+				participants.erase(begin);
+			}
+
+			break;
+#pragma endregion
+#pragma region case3 Изменение
+		case 3:
+			system("cls");
+			PrintList(participants);
+			cout << "Введите номер человека информацию о котром фы хотите изменить";
+			numb = base.correctInput();
+			begin = participants.begin();
+			cout << "Введите информцию о новом участнике\nИмя" << endl;
+			cin >> name;
+			cout << "Возраст\n";
+			age = base.correctInput();
+			cout << "Вид спорта\n";
+			cin >> kindOfsport;
+			addParticipants.setInfo(name, kindOfsport, age);
+			for (int i = 0; i <= numb - 2; i++, begin++);
+			begin = participants.erase(begin);
+			participants.insert(begin, addParticipants);
+			break;
+#pragma endregion
+#pragma region case4 Поиск
+		case 4:
+			system("cls");
+			cout << "Введите имя\n";
+			cin >> name;
+			begin = participants.begin();
+			for (int i = 0; i < participants.size(); i++, begin++)
+			{
+				if (begin->name == name)
+				{
+					cout << *begin << endl;
+				}
+			}
+			break;
+#pragma endregion
+#pragma region case5 Сортировка
+		case 5:
+			system("cls");
+			participants.sort();
+			cout << "Список успешно отсортирован)" << endl;
+			break;
+#pragma endregion	
+		case 6:
+			begin = participants.begin();
+			for (int i = 0; i < participants.size(); i++, begin++)
+			{
+				out << begin->name << "\n" << begin->age << "\n" << begin->kindOfSport << "\n";
+			}
+			cout << "Информация об участниках успешна записана\n";
+			break;
+		case 7:
+			system("cls");
+			PrintList(participants);
+			break;
+		case 0:
+			end = false;
+			break;
+		default:
+			cout << "Error\n";
+			break;
+		}
+	}
+#pragma endregion*/ 	
+}
+
+
+
--- Лабы/help/help.cpp
+++ Лабы/help/help.cpp
@@ -1,45 +1,40 @@
-﻿#include <fstream>
-#include <iostream>
+﻿#include <iostream>
+#include <map>    //подключили библиотеку для работы с map
 using namespace std;
-#include <fstream>
-#include <iostream>
-using namespace std;
-class Person {
-protected:
-	string name;//имя
-	int age;//возраст
-public:
-	void getData()
-	{//получить данные о человеке
-		cout << "\nВведите имя: ";
 
-		cin >> name;
-		cout << "Введите возраст: ";
-		cin >> age;
-	}
-	void showData()
-	{//вывод данных о человеке
-		cout << "\n Имя: " << name;
-		cout << "\n Возраст: " << age;
-	}
-};
-int main() {
-	system("chcp 1251");
-	system("cls");
-	Person pers;//создать объект person
-	ifstream infile;//создать входной файл
-	infile.open("GROUP.DAT", ios::in | ios::binary);
-	infile.seekg(0, ios::end);//установить указатель на 0 байт отконца файла
-	int endposition = infile.tellg();//найти позицию в файле
-	int n = endposition / sizeof(Person);//число человек
-	cout << "\nВ файле " << n << " человек(а)";
-	cout << "\nВведите номер персоны: ";
-	cin >> n;
-	int position = (n - 1) * sizeof(Person);//умножить размерсозданных данных на число персон
-	infile.seekg(position);//число байт от начала
-		//прочитать одну персону
-	infile.read(reinterpret_cast<char*>(&pers), sizeof(pers));
-	pers.showData();//вывести одну персону
-	cout << endl;
-	return 0;
+template <typename T,typename U>
+void p(map<T,U>& myFirstMap)
+{
+    for (auto it = myFirstMap.begin(); it != myFirstMap.end(); ++it)
+    {
+        cout << it->first << " : " << it->second << endl;
+    }
+}
+
+int main()
+{
+    map <string, int> myFirstMap = { { "Mother", 37 },
+                                   { "Father", 40 },///map явно инициализирована
+                                   { "Brother", 15 },
+                                   { "Sister", 20 } };
+    p(myFirstMap);
+    /*///вывод явно инициализированной map на экран
+    for (auto it = myFirstMap.begin(); it != myFirstMap.end(); ++it)
+    {
+        cout << it->first << " : " << it->second << endl;
+    }
+
+    char c;
+    map <char, int> mySecondMap;
+    for (int i = 0, c = 'a'; i < 5; ++i, ++c)
+    {
+        mySecondMap.insert(pair<char, int>(c, i));
+    }
+
+    ///вывод не явно инициализированной map на экран
+    for (auto it = mySecondMap.begin(); it != mySecondMap.end(); ++it)
+    {
+        cout << (*it).first << " : " << (*it).second << endl;
+    }*/
+    return 0;
 }
